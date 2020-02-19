635507e2975f08e15b5f8f2fa0c4ef805a0cb52d
Ajay <ajayvishwanath.94@gmail.com>
2020-02-18 16:50:10 -0800

    Error Component Drafted

--- frontend/src/components/ErrorDisplay.js
+++ frontend/src/components/ErrorDisplay.js
@@ -1,1 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
+import '../styles/error.scss';
+
+function Error(props){
+    return (
+        <div id="error-message-container">
+            <p>
+                {props.error}
+            </p>
+        </div>
+    )
+}
+
+export default Error;
