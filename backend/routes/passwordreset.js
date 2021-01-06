174d84f0a867e80d568a29563efb8ef57fb633d9
Attila Kovacs <akovacs101386@gmail.com>
2021-01-06 11:24:12 +0000

    Email sending POC

--- backend/routes/passwordreset.js
+++ backend/routes/passwordreset.js
@@ -5,6 +5,7 @@ const parser = require('koa-body');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const nodemailer = require("nodemailer");
+const config = require('../config');
 const knex = require('knex');
 const PasswordReset = require('../models/passwordreset');
 
@@ -24,31 +25,33 @@ passwordReset.get('/', async ctx => {
 passwordReset.post('/', parsers.json, async ctx => {
    let entryToInsert = ctx.request.body;
    let resetToken = crypto.randomBytes(128).toString('hex');
-   // TODO secret salt
-   const salt = '$2a$10$arjAldmQvHFfmUeL1/GCm.';
-   entryToInsert.resetHash = bcrypt.hashSync(resetToken, salt);
-   ctx.body = await PasswordReset.query()
-   .insert(entryToInsert)
-   .returning('*');
+    // TODO secret salt
+    const salt = '$2a$10$arjAldmQvHFfmUeL1/GCm.';
+    entryToInsert.resetHash = bcrypt.hashSync(resetToken, salt);
+    ctx.body = await PasswordReset.query()
+        .insert(entryToInsert)
+        .returning('*');
 
-  // let transporter = nodemailer.createTransport({
-  //   host: "smtp.ethereal.email",
-  //   port: 587,
-  //   secure: false, // true for 465, false for other ports
-  //   auth: {
-  //     user: testAccount.user, // generated ethereal user
-  //     pass: testAccount.pass, // generated ethereal password
-  //   },
-  // });
+    let transporter = nodemailer.createTransport({
+        host: config.exposed.get('email.smtp_host'),
+        port: config.exposed.get('email.smtp_port'),
+        secure: true,
+        auth: {
+            user: config.exposed.get('email.noreply_email'),
+            pass: config.secret.get('dreamhost_smtp_password')
+        }
+    });
 
-  // let info = await transporter.sendMail({
-  //   from: '"Fred Foo 👻" <foo@example.com>', // sender address
-  //   to: "bar@example.com, baz@example.com", // list of receivers
-  //   subject: "Hello ✔", // Subject line
-  //   text: "Hello world?", // plain text body
-  //   html: "<b>Hello world?</b>", // html body
-  // });
-});
+    let info = await transporter.sendMail({
+        from: config.exposed.get('email.noreply_email'),
+        to: ctx.request.body.requesterEmail,
+        subject: "Hello ✔",
+        text: "Pw reset",
+        html: "<b>Pw reset test</b>"
+    });
+
+    console.log("Message sent: %s", info.messageId);
+ });
 
 passwordReset.get('/:id', async ctx => {
   ctx.body = await PasswordReset.query().findById(ctx.params.id);
