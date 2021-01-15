3a1a2d6e59632dbda06cfa4f40f5c3a374be15f8
Attila Kovacs <akovacs101386@gmail.com>
2021-01-15 22:33:16 +0000

    Changed is_verified default DB value to false

--- backend/migrations/20210115222728_change_is_Verified_default_to_false.js
+++ backend/migrations/20210115222728_change_is_Verified_default_to_false.js
@@ -3,13 +3,13 @@
 
 async function up(knex) {
     await knex.schema.alterTable('users', function(t) {
-        t.renameColumn('active_p', 'is_verified')
+        t.boolean('is_verified').notNullable().defaultTo(false).alter();
     });
 };
 
 async function down(knex) {
     await knex.schema.alterTable('users', function(t) {
-        t.renameColumn('is_verified', 'active_p')
+        t.boolean('is_verified').notNullable().defaultTo(true).alter();
     });
 };
 
