c49ebb608502507af87b6a7af4a8ae5fbf27ccca
Attila Kovacs <akovacs101386@gmail.com>
2021-01-19 10:42:10 +0000

    Login API touchup - backend

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -2,44 +2,44 @@ const passport = require('koa-passport');
 const LocalStrategy = require('passport-local').Strategy;
 const knex = require('../lib/knex.js');
 const bcrypt = require('bcryptjs');
-const PwResets = require('../models/pwresets');
 
 //Passport functions
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  knex('users').where({
-    id: id
-  }).then((user) => {
-    done(null, user);
-  });
+    knex('users').where({
+        id: id
+    }).then((user) => {
+        done(null, user);
+    });
 });
 
 const strategyOptions = {};
 
 passport.use(new LocalStrategy(strategyOptions, (username, password, done) => {
-  knex('users').where({
-    username: username
-  }).first()
-  .then((user) => {
-    if (!user) {
-        return done(null, false);
-    }
-    if (password === user.pw_hash) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
-  })
-  .catch((err) => { return done(err); });
+    knex('users').where({
+        username: username
+    }).first()
+        .then((user) => {
+            if (!user) {
+                return done(null, false);
+            }
+            const incomingPwHash = bcrypt.hashSync(password, user.pw_salt);
+            if (incomingPwHash === user.pw_hash) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch((err) => { return done(err); });
 }));
 
 let Auth = {};
 
 Auth.doLogin = function () {
-  return 0;
+    return 0;
 }
 
 module.exports = Auth;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,13 +2,12 @@
 
 const Router = require('koa-router');
 const parser = require('koa-body');
-
 const passport = require('koa-passport');
 
 const parsers = {
-  query: parser({urlencoded: true, multipart: false, json: false}),
-  form: parser({urlencoded: false, multipart: true, json: false}),
-  json: parser({urlencoded: false, multipart: false, json: true}),
+    query: parser({urlencoded: true, multipart: false, json: false}),
+    form: parser({urlencoded: false, multipart: true, json: false}),
+    json: parser({urlencoded: false, multipart: false, json: true}),
 };
 
 const auth = new Router();
@@ -18,27 +17,32 @@ auth.get('/', parsers.json, async ctx => {
 });
 
 auth.post('/login', parsers.json, async ctx => {
-  return passport.authenticate('local', (err, user, info, status) => {
-    if (user) {
-      ctx.login(user);
-      ctx.status = 200;
-      ctx.body = user;
-    } else {
-      ctx.status = 400;
-      ctx.body = 'Bummer!';
-    }
-  })(ctx);
+    return passport.authenticate('local', (err, user, info, status) => {
+        let response = {};
+        if (user) {
+            ctx.login(user);
+            ctx.status = 200;
+            response.result = 'success';
+            response.username = user.username;
+            ctx.body = JSON.stringify(response);
+        } else {
+            ctx.status = 400;
+            response.result = 'error';
+            response.error = 'Generic error at login';
+            ctx.body = JSON.stringify(response);
+        }
+    })(ctx);
 });
 
 auth.post('/logout', parsers.json, async ctx => {
-  if (ctx.isAuthenticated()) {
-    ctx.logout();
-    ctx.status = 200;
-    ctx.body = 'Logout successful';
-  } else {
-    ctx.body = { success: false };
-    ctx.throw(401);
-  }
+    if (ctx.isAuthenticated()) {
+        ctx.logout();
+        ctx.status = 200;
+        ctx.body = 'Logout successful';
+    } else {
+        ctx.body = { success: false };
+        ctx.throw(401);
+    }
 });
 
 module.exports = auth;
