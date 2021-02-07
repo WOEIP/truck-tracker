42c9c91063112d01e3bc7a4305d87971d70de3e9
Ajay <ajayvishwanath.94@gmail.com>
2021-02-07 15:15:06 -0800

    Add Password Confirm + Error Messaging to Registration Component

--- frontend/src/components/PasswordResetLanding.js
+++ frontend/src/components/PasswordResetLanding.js
@@ -10,52 +10,53 @@ class PasswordResetLanding extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            password: '',
-            passwordConfirm: '',
-            error: ''
+            password: "",
+            passwordConfirm: "",
+            error: "",
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.setPassword = this.setPassword.bind(this);
     }
 
-    setPassword (event) {
+    setPassword(event) {
         // TODO disable send button when there is no password
         // TODO instead of preventDefault, we could maybe track down what catches the event?
-        event.preventDefault()
+        event.preventDefault();
 
-        if(this.state.password !== this.state.passwordConfirm){
+        if (this.state.password !== this.state.passwordConfirm) {
             this.setState({
-                error: "Passwords do not match. Please re-enter and try again."
-            })
+                error: "Passwords do not match. Please re-enter and try again.",
+            });
         } else {
+            const urlParams = new URLSearchParams(window.location.search);
+            const confirmationToken = urlParams.get("token");
 
-       const urlParams = new URLSearchParams(window.location.search);
-       const confirmationToken = urlParams.get('token');
+            let postData = {
+                password: this.state.password,
+                resetToken: confirmationToken,
+            };
 
-        let postData = {
-            password: this.state.password,
-            resetToken: confirmationToken
-        };
-
-        Api.post('passwordreset/execute', postData).then(response => {
-            if (response.status === 200) {
-                //TODO message
-                window.location.hash = "#login";
-                console.log(response);
-            }
-        }).catch((error) => {
-            // TODO
-            console.log(error);
-        });
-    }
+            Api.post("passwordreset/execute", postData)
+                .then((response) => {
+                    if (response.status === 200) {
+                        //TODO message
+                        window.location.hash = "#login";
+                        console.log(response);
+                    }
+                })
+                .catch((error) => {
+                    // TODO
+                    console.log(error);
+                });
+        }
     }
 
     handleInputChange(inputField, evt) {
         let newState = this.state;
         newState[inputField] = evt.target.value;
         this.setState(newState);
-    };
+    }
 
     render() {
         const errors = this.state.error ? (
@@ -64,23 +65,28 @@ class PasswordResetLanding extends Component {
 
         return (
             <article id="password-reset-landing">
-              <Menu current="login"/>
-              {errors}
-              <form>
-                <label>New password</label>
-                <input type="password"
-                       value={this.state.password}
-                       onChange={this.handleInputChange.bind(this, 'password')} />
-                <label>Confirm new password</label>
-                <input type="password"
-                       value={this.state.passwordConfirm}
-                       onChange={this.handleInputChange.bind(this, 'passwordConfirm')} />
-                <div className="actions">
-                  <button onClick={this.setPassword}>
-                    Send
-                  </button>
-                </div>
-              </form>
+                <Menu current="login" />
+                {errors}
+                <form>
+                    <label>New password</label>
+                    <input
+                        type="password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange.bind(this, "password")}
+                    />
+                    <label>Confirm New Password</label>
+                    <input
+                        type="password"
+                        value={this.state.passwordConfirm}
+                        onChange={this.handleInputChange.bind(
+                            this,
+                            "passwordConfirm"
+                        )}
+                    />
+                    <div className="actions">
+                        <button onClick={this.setPassword}>Send</button>
+                    </div>
+                </form>
             </article>
         );
     }
