1cefab8092364614d9847f0a12dba1bfd978c74d
Jess Sand <32755410+theecrit@users.noreply.github.com>
2021-02-10 13:48:03 -0800

    Content and layout updates
    About:
    - Swaps out depressing train depot pic with a WO mural :)
    - Updates copy to focus on value > scientific background
    
    Contibuting:
    - Minor copy updates to simplify language and add clear CTAs
    
    Menu:
    - Updates About label
    
    Home:
    - Moves menu to top of viewport for easier access
    - Updates project name (may need to be updated again at some point)
    - Updates beta copy.

--- frontend/src/components/Contributing.js
+++ frontend/src/components/Contributing.js
@@ -13,22 +13,18 @@ class Contributing extends Component {
     return (
       <article id="about-text">
         <Menu current="about"/>
-        <h1 className="title">Help us creating a tool for empowerment!</h1>
-        <p> Collecting and visualizing data is an efficient method to influence
-            policymakers who can make decisions to create a better living
-            environment. By making truck traffic data available,
-            we hope to give underrepresented
-            communities a voice and make it possible to stand up for themselves.
-            For more details on this, check out our <a className="textlink"
-            href="#about">mission description.</a>
+        <h1 className="title">Help build tools for community resilience</h1>
+        <p> Collecting and visualizing data is an important way to influence
+            those who make the policies that impact the neighborhodds we live in. Truck Tracker gives underrepresented
+            communities more than a seat at the table; it gives us a voice.
         </p>
-        <p> Because we receive no funding for this project, we rely heavily on
-            volunteer work. Right now we're looking for a person who can help
-            us to further develop this web app, adding functionality and doing
-            maintenance. The project is at a stage where we will coordinate with
-            other NGOs in Oakland and provide the app first to research groups, then
-            to the general public. This is a great time to join, to have some
-            fun while gathering experience and making an impact.
+        <p align="center"><a className="button" href="#contact">Volunteer</a></p>
+
+        <p> Rooted in our principles of <a className="textlink" href="https://woeip.org/our-approach/">collaborative problem-solving</a>, our team relies heavily on
+            the contributions of those care most about these issues. Right now we're looking for someone with technical experience who can help
+            us continue to improve Truck Tracker, adding functionality and doing
+            maintenance. We're currently testing the app with local advocates in West Oakland and will then
+            extend our reach to the general public. This is a great time to join!
         </p>
         <p> The ideal candidate has some programming experience and is eager to
             learn. Because this is an underpaid commitment, you'll have great
@@ -37,8 +33,7 @@ class Contributing extends Component {
             using React, tweaking the backend architecture (koa.js), fiddling with
             the DB layer (knex.js) or doing infrastructure config (nginx, node). If
             you search the web for these keywords, you'll find that our technology
-            stack is consistent with industry standards. This is a great opportunity for a
-            good reference and great real world experience in the web field. We will make our
+            stack is consistent with industry standards. We will make our
             best effort to provide you guidance and mentoring so you don't feel
             lost while having a healthy amount of responsibility.
         </p>
@@ -47,11 +42,10 @@ class Contributing extends Component {
            your valuable time and spread the word about this volunteer opportunity. Do check out the code at
            <a className="textlink" href="https://github.com/WOEIP/truck-tracker/"> our repo </a>
            (if you know a bit of Javascript you'll see that we try to follow best practices
-            but that we are working with limited worker hours available to build this prototype,
-            that's where you come in ;) )
+            but that we are working with limited worker hours available to build this prototype—that's where you come in ;) ).
         </p>
         <p>
-           With inquiries be in touch via motching at gmail dot com.
+           <a className="textlink" href="#contact">Let us know you're interested.</a>
         </p>
         <p>
             Thank you!
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -2,26 +2,16 @@ import React, { Component } from 'react';
 import '../styles/main-page.scss';
 import '../styles/pure-release-1.0.0/pure-min.css';
 import '../styles/pure-release-1.0.0/grids-responsive.css';
-import logo from '../img/logo.png';
+import logo from '../img/logo-200p.png';
 
 class MainPage extends Component {
   render() {
     return (
       <div id="app-container">
         <div id="logo-container">
-          <img src={logo} alt="logo"/>
+          <img className="logo-home" src={logo-200p} alt="WOEIP logo"/>
         </div>
-        <div className="vert-space long line"></div>
-        <div id="inner-title">
-          <h1>WOEIP Truck Report System</h1>
-        </div>
-        <div className="vert-space short"></div>
-        <div id="beta-testing-message-container">
-          <p>This app is under beta testing, you may assume there will be glitches.
-            Please report problems you find by sending an email to motching
-            at gmail dot com. Thank you!
-          </p>
-        </div>
+
         <div className="vert-space short"></div>
         <nav className="pure-g main-menu">
           <a className="pure-u-1 pure-u-sm-1-4" href="#report">Report</a>
@@ -30,6 +20,18 @@ class MainPage extends Component {
           <a className="pure-u-1 pure-u-sm-1-4" href="#contact">Contact</a>
         </nav>
         <div className="vert-space short"></div>
+
+        <div id="inner-title">
+          <h1>Truck Tracker</h1>
+          <h2 className="title">A WOEIP project</h2>
+        </div>
+        <div className="vert-space short"></div>
+        <div id="beta-testing-message-container">
+          <p>Truck Tracker is currently in early release and you may encounter issues. Please <a className="textlink" href="#contact">let us know</a> if you have trouble using the site or have feedback to help us improve.
+          </p>
+        </div>
+
+
       </div>
     );
   }
