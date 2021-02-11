21ffc07808513d25c38390c452df0957a1cb6eff
Jess Sand <32755410+theecrit@users.noreply.github.com>
2021-02-10 16:48:43 -0800

    Accessibility and copy updates
    - Updated menu, text, and link colors/styles to increase contrast
    - Simplified Contributing copy and provided clearer CTAs.
    - Adjusted home page layout for

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -16,7 +16,7 @@ class About extends Component {
       <article id="about-text">
         <Menu current="about"/>
         <h1 className="title">About Truck Tracker</h1>
-        <p><b>Truck Tracker enables West Oakland residents to monitor truck traffic, so we can better understand the impact of trucking emissions on our community, hold violators accountable, and advocate for better air.</b></p>
+        <p><b>Truck Tracker helps West Oakland residents monitor truck traffic, so we can better understand the impact of trucking emissions on our community, hold violators accountable, and advocate for better air.</b></p>
         <span className="image main"><img src={mural} alt="aerial view of abnadoned 16th Street train depot in West Oakland" /></span>
         <p>West Oakland is surrounded by three major freeways, sits next to the Port of Oakland, and is home to a major U.S. Postal Service Distribution Center. This brings a <i>lot</i> of trucks into the neighborhood day in and day out. These trucks expose our neighborhood to high concentrations of diesel particulate matter—almost three times higher than the Bay Area average. Trucks remain the single highest source of diesel emissions here. </p>
         <span className="image main"><img src={truck} alt="Long view of a gas tanker truck" /></span>
@@ -26,7 +26,7 @@ class About extends Component {
         <p>Diesel exhaust has been classified a potential human carcinogen by the U.S. Environmental Protection Agency (EPA) and the International Agency for Research on Cancer. Exposure to high levels of diesel exhaust has been shown to cause lung tumors in rats, and studies of humans routinely exposed to diesel fumes indicate a greater risk of lung cancer. For example, occupational health studies of railroad, dock, trucking, and bus garage workers exposed to high levels of diesel exhaust over many years consistently demonstrate a 20 to 50 percent increase in the risk of lung cancer or mortality.</p>
         <h2 className="title">Get involved</h2>
         <p>There are plenty of ways you can help West Oakland advocate for cleaner air:
-        <ul><li>Create a free Truck Tracker account and <a href="#report">report illegal truck activity</a> when you see it.</li>
+        <ul><li>Use Truck Tracker to <a className="textlink" href="#report">report illegal truck activity</a> when you see it.</li>
         <li>Learn about <a className="textlink" href="https://woeip.org/featured-work/owning-our-air/">Owning Our Air</a>, WOIEP's initiative to hold the City and County accountable for reducing toxic air exposure in West Oakland.</li>
         <li>Contribute your coding, research, or other skills to the work we're doing. Just <a className="textlink" href="#contact">send us a note</a> to let us know what you're most interested in.</li></ul></p>
 
--- frontend/src/components/Contributing.js
+++ frontend/src/components/Contributing.js
@@ -15,41 +15,25 @@ class Contributing extends Component {
         <Menu current="about"/>
         <h1 className="title">Help build tools for community resilience</h1>
         <p> Collecting and visualizing data is an important way to influence
-            those who make the policies that impact the neighborhodds we live in. Truck Tracker gives underrepresented
+            those who make the policies that impact the neighborhoods we live in. Truck Tracker gives underrepresented
             communities more than a seat at the table; it gives us a voice.
         </p>
         <p align="center"><a className="button" href="#contact">Volunteer</a></p>
 
         <p> Rooted in our principles of <a className="textlink" href="https://woeip.org/our-approach/">collaborative problem-solving</a>, our team relies heavily on
-            the contributions of those care most about these issues. Right now we're looking for someone with technical experience who can help
+            the contributions of those care most about these issues. Right now we're looking for a volunteer with software development experience who can help
             us continue to improve Truck Tracker, adding functionality and doing
-            maintenance. We're currently testing the app with local advocates in West Oakland and will then
+            maintenance. We're currently testing the app with local advocates in West Oakland and will eventually
             extend our reach to the general public. This is a great time to join!
         </p>
         <p> The ideal candidate has some programming experience and is eager to
-            learn. Because this is an underpaid commitment, you'll have great
-            freedom in choosing what to do. There are tasks of varying time commitment;
-            creating frontend design with SCSS, doing application logic
-            using React, tweaking the backend architecture (koa.js), fiddling with
-            the DB layer (knex.js) or doing infrastructure config (nginx, node). If
-            you search the web for these keywords, you'll find that our technology
-            stack is consistent with industry standards. We will make our
-            best effort to provide you guidance and mentoring so you don't feel
-            lost while having a healthy amount of responsibility.
+            learn about using technology for community impact. Whether you're available for a few hours a month or a few hours a week, we'd love to have your help.
         </p>
-        <p>
-           If you've read this far, thanks! Please consider helping us out with
-           your valuable time and spread the word about this volunteer opportunity. Do check out the code at
-           <a className="textlink" href="https://github.com/WOEIP/truck-tracker/"> our repo </a>
-           (if you know a bit of Javascript you'll see that we try to follow best practices
-            but that we are working with limited worker hours available to build this prototype—that's where you come in ;) ).
-        </p>
-        <p>
-           <a className="textlink" href="#contact">Let us know you're interested.</a>
-        </p>
-        <p>
+          <p>
             Thank you!
         </p>
+        <p align="center"><a className="button" href="#contact">Get in touch</a></p>
+
       </article>
     );
   }
