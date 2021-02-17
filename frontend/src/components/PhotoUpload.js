1f77fda8f7c85cc861f0498a2a8b182dceb95417
Attila Kovacs <akovacs101386@gmail.com>
2021-02-17 01:25:51 -0800

    I58: Dropzone frontend stub

--- frontend/src/components/PhotoUpload.js
+++ frontend/src/components/PhotoUpload.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import LoadingButton from "./LoadingButton";
 import "../styles/loading-button.scss";
+import Dropzone from 'dropzone';
 
 class PhotoPage extends Component {
-
     constructor(props){
         super(props);
-
         this.sendData = this.sendData.bind(this);
     }
 
+  componentDidMount() {
+      let dropzoneConfig = {
+          url: 'hello'
+      }
+      let dropzoneInstance = new Dropzone('div#dropzone-container', dropzoneConfig);
+  }
+
     sendData(e){
         e.preventDefault();
-
         this.props.sendData()
     }
 
@@ -24,6 +29,7 @@ class PhotoPage extends Component {
                     <p className="helptext">
                         Coming soon...
                     </p>
+                    <div id="dropzone-container"></div>
                     <div className="dot-actions">
                         <LoadingButton onClick={this.sendData} loading={this.props.loading} label="Submit"/>
                     </div>
